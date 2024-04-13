2dc8f38ebb1848c3381e7b704bd8d20c0a6cfc68
Divas Kumar <divas.kumar@byjus.com>
2024-04-14 00:07:44 +0530

    added comments

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -51,20 +51,22 @@ const userSchema = new mongoose.Schema(
 
 // middleware function in a Mongoose schema, specifically a pre-save hook. It is used to hash the password before saving it to the database.
 userSchema.pre("save", async function (next) {
-  // if password is not modified then simply return to execute next method;
+  // checks whether the password field (this.password) has been modified. If the password has not been modified, it means it is either an update operation where the password is not being changed or it's a new document being created without any modifications to the password field. 
   if (!this.isModified(this.password)) {
     return next();
   }
 
-  // encrypt the password using bcrypt
+  // If the password has been modified, meaning it's a new password or an updated one, this part hashes the password using bcrypt.
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
+// defines a new method called isPasswordCorrect on the schema's methods object. isPasswordCorrect method allows you to check if a given plain-text password matches the hashed password stored in a user document. It's typically used during authentication to verify user credentials.
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
+// defines a new method called isPasswordCorrect on the schema's methods object.
 userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     {
@@ -80,6 +82,7 @@ userSchema.methods.generateAccessToken = function () {
   );
 };
 
+// defines a new method called isPasswordCorrect on the schema's methods object.
 userSchema.methods.generateRefreshToken = function () {
   return jwt.sign(
     {
