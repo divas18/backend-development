0a88b1dc1f9d60b8e44aa221af9f93708ab8fbc3
Divas Kumar <divas.kumar@byjus.com>
2024-04-14 23:53:11 +0530

    added routes & functionality for login + logout

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,30 +4,48 @@ import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
-const registerUser = asyncHandler(async (request, response, next) => {
-  //   response.status(200).json({
-  //     message: "ok",
-  //   });
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
 
-  // get details from frontend
-  // validation (check for empty field)
-  // check if user already exist: username or email
-  // check images, check for avatar
-  // upload them to cloudinary, avatar
-  // create user object- create entry in db
-  // remove password and refresh token field from response
-  // check user creation
-  // return response
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating refresh token"
+    );
+  }
+};
+
+const registerUser = asyncHandler(async (request, response, next) => {
+  /*
+  Todos
+    1. get details from frontend
+    2. validation (check for empty field)
+    3. check if user already exist: username or email
+    4. check images, check for avatar
+    5. upload them to cloudinary, avatar
+    6. create user object- create entry in db
+    7. remove password and refresh token field from response
+    8. check user creation
+    9. return response
+  */
 
   const { fullName, email, username, password } = request.body;
 
   // validation
-  if (Object.keys(request.body).length === 0 ||
+  if (
+    Object.keys(request.body).length === 0 ||
     [fullName, email, username, password].some((field) => field?.trim === "")
   ) {
     throw new ApiError(400, "All feild is required");
   }
-  // Checking for existed user
+  // Checking for existed user using this syntax we can check for username or email both at same time
   const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
@@ -44,7 +62,11 @@ const registerUser = asyncHandler(async (request, response, next) => {
   }
 
   let coverImageLocalPath;
-  if(request.files && Array.isArray(request.files.coverImage) && request.files.coverImage.length > 0) {
+  if (
+    request.files &&
+    Array.isArray(request.files.coverImage) &&
+    request.files.coverImage.length > 0
+  ) {
     coverImageLocalPath = request.files.coverImage[0].path;
   }
 
@@ -78,4 +100,91 @@ const registerUser = asyncHandler(async (request, response, next) => {
     .json(new ApiResponse(200, createdUser, "user register successfully"));
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (request, response, next) => {
+  /* 
+    Todo's 
+    1. get data from frontend
+    2. validate the information provided by user
+    3. check user is available or not on DB (username or email)
+    4. check password is correct password provided by user or not
+    5. password is correct Login & access & refreshtoken generate otherwise throw error wrong password
+    6. send cookies
+  */
+
+  const { email, username, password } = request.body;
+
+  if (!username || !email) {
+    throw new ApiError(400, "username or password is required");
+  }
+
+  // Check whether username or email exist in DB
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid user credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return response
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          refreshToken,
+          accessToken,
+        },
+        "User logged in successfully"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (request, response, next) => {
+  await User.findByIdAndUpdate(
+    request.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return response
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User Logged Out Successfully"));
+});
+
+export { registerUser, loginUser, logoutUser };
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,34 @@
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler(async (request, _, next) => {
+  try {
+    const token =
+      request.cookie?.accessToken ||
+      request.header("Authorization")?.replace("Bearer ", "");
+
+    if (!token) {
+      throw new ApiError(401, "Unauthorized Request");
+    }
+
+    const decodedToken = await jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET
+    );
+
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      throw new ApiError(401, "Invalid access token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid access token");
+  }
+});
